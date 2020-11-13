991d4f0a71c9879f5df30aab185444c268269030
Rohan Dutt <duttrohan0302@gmail.com>
2020-11-13 22:07:02 +0530

    TOPSIS Assignment

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TOPSIS-Rohan-101803151",
+    version="1.0.0",
+    author="",
+    author_email="",
+    description="A package that makes it easy to create Pypi packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+          'pandas',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+) 
