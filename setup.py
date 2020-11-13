cafde3d5200b9626ab8df3a593b571f913f95ed6
Rohan Dutt <duttrohan0302@gmail.com>
2020-11-13 22:15:47 +0530

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TOPSIS-Rohan-101803151",
-    version="1.0.0",
+    version="1.0.1",
     author="",
     author_email="",
     description="A package that makes it easy to create Pypi packages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url = 'https://github.com/duttrohan0302/TOPSIS-Rohan-101803151',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/duttrohan0302/TOPSIS-Rohan-101803151/archive/v_01.tar.gz',
     packages=setuptools.find_packages(),
     install_requires=[
           'numpy',
